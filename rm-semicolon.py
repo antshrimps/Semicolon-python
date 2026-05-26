68b27244ca059a82b86af12879bdd6391e8f2be7
Antshrimps <maximilian.muydinov@gmail.com>
2026-05-26 15:17:06 +0200

    Update rm-semicolon.py
    changing location of scripts

--- rm-semicolon.py
+++ rm-semicolon.py
@@ -3,18 +3,7 @@
 from sys import argv
 from pathlib import Path
 
-#try:
-#    filename = argv[1]
-#except:
- #   outputfile = Path(f"/home/maxi/stuff/{filename}.shadow.py")
-  #  output_file.parent.mkdir(exist_ok=True, parents=True)
-#
- #   with open outputfile as f:
-  #      f.write("#!EMPTY)
-
 filename = argv[1]
-
-##print("Removing semicolons from" + filename)
 
 with open(filename, 'r') as f:
     nfile = ""
@@ -22,7 +11,7 @@ with open(filename, 'r') as f:
         if sym != ';':
             nfile += sym
 
-outputfile = Path(f"/home/maxi/stuff/{filename}.shadow.py")
+outputfile = Path(f"~/.semicolon-python/{filename}.shadow.py")
 outputfile.parent.mkdir(exist_ok=True, parents=True)
 
 with open(outputfile, 'w') as f:
