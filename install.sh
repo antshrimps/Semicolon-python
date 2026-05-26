454d75a91f94fd05fd072e7695777f4417f7ecf6
Antshrimps <maximilian.muydinov@gmail.com>
2026-05-26 16:04:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.semicolon-python
 
-cp -rf rm-semicolon.py ~/.semicolon-python/
-mv rm-semicolon.py rm-semicolon.py.backup
+mv rm-semicolon.py ~/.semicolon-python/
+#mv rm-semicolon.py rm-semicolon.py.backup
 
 chmod a+x python4
