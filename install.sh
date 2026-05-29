141348a9d4c58e2ce6ba1cf0504930f4ae115409
Antshrimps <maximilian.muydinov@gmail.com>
2026-05-29 21:02:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo apt install python3-rich
 
 mkdir -p ~/.semicolon-python/
 
-mv terminal.py ~/.semicolon-python/
+cp terminal.py ~/.semicolon-python/
 
-mv rm-semicolon.py ~/.semicolon-python/
+cp rm-semicolon.py ~/.semicolon-python/
 #mv rm-semicolon.py rm-semicolon.py.backup
 
 chmod a+x python4
