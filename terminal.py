0e2795979ce4fad7f8b3ba8b418171f7d343c81f
Antshrimps <maximilian.muydinov@gmail.com>
2026-05-29 20:58:58 +0200

    Add files via upload

---
+++ terminal.py
@@ -0,0 +1,17 @@
+from rich.console import Console
+
+con = Console()
+
+comm = None
+
+while True:
+    #print(f"##### {comm} #####")
+    if comm == "quit" or comm == "exit":
+        break
+    comm = con.input("[magenta]>>> ")
+    comm = comm.replace(';', '')
+    try:
+        exec(comm)
+    except Exception as e:
+        print(e)
+
