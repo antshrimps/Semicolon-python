1c2081faf83d3e7ce1e0621211489772bedcd272
Antshrimps <maximilian.muydinov@gmail.com>
2026-05-29 21:13:48 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -4,6 +4,10 @@ con = Console()
 
 comm = None
 
+print('Python 4.1.3 (main, Mar 15 2026, 11:30:27) [GCC 15.2.0] on linux')
+print('Type "help", "copyright", "credits" or "license" for more information.')
+
+
 while True:
     if comm == "quit" or comm == "exit":
         break
