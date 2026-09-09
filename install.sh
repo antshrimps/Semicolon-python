a7fd2e5c33e5bf33315590a6842d6845b54abc78
Antshrimps <maximilian.muydinov@gmail.com>
2026-09-09 19:49:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ echo if you are not on a debian based distro change the install in install.sh
 
 sudo apt install python3-rich
 
-mkdir -p ~/.semicolon-python/
+sudo mkdir -p ~/.semicolon-python/
 
-cp terminal.py ~/.semicolon-python/
+sudo cp terminal.py ~/.semicolon-python/
 
-cp rm-semicolon.py ~/.semicolon-python/
-mv rm-semicolon.py rm-semicolon.backup.py
+sudo cp rm-semicolon.py ~/.semicolon-python/
+sudo mv rm-semicolon.py rm-semicolon.backup.py
 
-chmod a+x python4
+sudo chmod a+x python4
 sudo mv python4 python4.backup
 
 echo moving python4 to /bin
